9edc92ff44e82871f76b59afddf189213722f381
jas- <jason.gerfen@gmail.com>
2014-09-01 14:53:31 -0600

    development Mon Sep  1 14:53:31 MDT 2014: Implemented arg list processing

--- src/libzmap.cc
+++ src/libzmap.cc
@@ -1,6 +1,8 @@
 #include <node.h>
 #include <v8.h>
 
+#include "zmap-1.2.1/src/zopt.h"
+
 #include "libzmap.h"
 
 namespace libzmap {
@@ -11,9 +13,39 @@ using namespace v8;
 Handle<Value> LibZMAP(const Arguments& args) {
   HandleScope scope;
 
-  Local<Object> obj = Object::New();
-  obj->Set(String::NewSymbol("x"), String::New("hello"));
-  obj->Set(String::NewSymbol("y"), String::New("world"));
+  Local<Function> callback;
+  Local<Object> obj;
+
+  if (args.Length() < 1) {
+    ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
+    return scope.Close(Undefined());
+  }
+
+  if (args[0]->IsFunction()) {
+    callback = Local<Function>::Cast(args[0]);
+  } else {
+    if (!args[1]->IsFunction()) {
+      ThrowException(Exception::TypeError(String::New("Function expected as second argument")));
+      return scope.Close(Undefined());
+    }
+
+    callback = Local<Function>::Cast(args[1]);
+
+    if (!args[0]->IsObject()) {
+      ThrowException(Exception::TypeError(String::New("Object expected as first argument")));
+      return scope.Close(Undefined());
+    }
+  }
+
+  if (args[0]->IsObject()) {
+    obj = args[0]->ToObject();
+  }
+
+  obj->Set(String::NewSymbol("x"),
+    String::New("wtf"));
+
+  /* Setup options */
+  /* Initialize scan */
 
   return scope.Close(obj);
 }
