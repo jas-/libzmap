e37dc2a70137fe2288526edbf5b4abb94bbcaebb
jas- <jason.gerfen@gmail.com>
2014-09-23 05:47:29 -0600

    misc notes

--- test/example.js
+++ test/example.js
@@ -8,7 +8,7 @@ var lib = require('../build/Release/zmap.node')
 //      shardtotal: 4,
       threads: 10,
 //      range: '155.97.0.0/17',
-      probemodule: 'udp', //tcp_synscan, udp, icmp_echoscan (default)
+//      probemodule: 'tcp_synscan', //tcp_synscan, udp, icmp_echoscan (default)
 //      blacklist: '/home/jas/projects/mine/libzmap/src/zmap-1.2.1/conf/blacklist.conf',
       whitelist: '/home/jas/projects/mine/libzmap/test/whitelist.conf'
     };
